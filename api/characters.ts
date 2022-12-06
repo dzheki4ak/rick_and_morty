e6317baae2c573d4b6ed59591a18a93af906b45f
Ihor Kychak <ihor.kychak@litslink.com>
2022-12-06 17:23:27 +0100

    Appolo client // Basic

---
+++ api/characters.ts
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const GET_CHARACTERS = gql`
+  query GetCharacters {
+    characters {
+      info {
+        count
+      }
+      results {
+        id
+        name
+        gender
+        image
+      }
+    }
+  }
+`;
---
+++ pages/types.ts
@@ -0,0 +1,6 @@
+export type Character = {
+  gender: 'string';
+  id: 'string';
+  image: 'string';
+  name: 'string';
+};
